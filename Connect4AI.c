e7d1081071b3e1d7edaf1bc277ec44fe406eed60
Michelle Li <michellecarolynclark@gmail.com>
2016-07-28 23:12:28 -0700

    added board array, wrote most of check winner function

--- Connect4AI.c
+++ Connect4AI.c
@@ -10,6 +10,11 @@
 #define yellowMin 40
 #define yellowMax 50
 
+// define global array variable to store the state of the board
+// 7 across, 6 down
+// 0 for empty, 1 for opponent, 2 for robot
+int board[7][6];
+
 void nextTurnSound()
 {
 	// beep
@@ -65,13 +70,62 @@ void playEndSound(bool winnerUser)
 	{
 		// play losing sound
 	}
+	// exit program here (or, if not possible, break out of all loops in main)
 }
 
 void checkWiner()
 {
+	bool opponentWon = true, robotWon = true;
 	// check horizontal
+	for (int row = 0; row <= 5; row++)
+	{
+		for (int colStart = 0; colStart <= 3; colStart++)
+		{
+			opponentWon = true, robotWon = true;
+			for (int i = 0; i < 4; i++)
+			{
+				if (board[row][colStart + i] != 1)
+				{
+					opponentWon = false;
+				}
+				if (board[row][colStart + i] != 2)
+				{
+					robotWon = false;
+				}
+			}
+		}
+	}
+	if (robotWon)
+	{
+		playEndSOund(false);
+	}
+	else if (opponentWon)
+	{
+		playEndSound(true);
+	}
 	// check vertical
-	// check diagonal
+	for (int col = 0; col <= 6; col++)
+	{
+		for (int rowStart = 0; rowStart <= 2; rowStart++)
+		{
+			opponentWon = true, robotWon = true;
+			for (int i = 0; i < 4; i++)
+			{
+				if (board[row][colStart + i] != 1)
+				{
+					opponentWon = false;
+				}
+				if (board[row][colStart + i] != 2)
+				{
+					robotWon = false;
+				}
+			}
+		}
+	}
+	// check top left to bottom right diagonal
+
+
+	// check bottom left to top right diagonal
 }
 
 void determinePieceLocation()
@@ -91,9 +145,7 @@ void determineNextMove()
 
 task main()
 {
+	// sync the two drive train motors because the robot only moves straight
 	setMotorSync(leftMotor, rightMotor, 0, 80);
-	// tests
-	nextTurnSound();
-	rotateArm();
 
 }
